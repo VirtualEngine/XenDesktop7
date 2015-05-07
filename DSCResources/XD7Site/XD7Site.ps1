7c16de3aab0b4f11501e32b4b1b09fcfa657d770
Iain Brighton <iainbrighton@hotmail.com>
2015-05-07 16:39:20 +0100

    Changed $Credential to a mandatory parameter for database creation Replaced InvokeScriptBlock with calls to Invoke-Command

--- DSCResources/XD7Site/XD7Site.ps1
+++ DSCResources/XD7Site/XD7Site.ps1
@@ -8,28 +8,42 @@ function Get-TargetResource {
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $DatabaseServer,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $SiteDatabaseName,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $LoggingDatabaseName,
-        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName,
+        [Parameter(Mandatory)] [ValidateNotNull()] [System.Management.Automation.PSCredential] $Credential
     )
     begin {
-        if (-not (TestModule -Name 'Citrix.XenDesktop.Admin')) {
+        if (-not (TestXDModule)) {
             ThrowInvalidProgramException -ErrorId 'Citrix.XenDesktop.Admin module not found.' -ErrorMessage $localizedData.XenDesktopSDKNotFoundError;
         }
     } #end begin
     process {
-        try {
-            ## ErrorAction is ignored :@
-            $xdSite = Get-XDSite -ErrorAction SilentlyContinue;
+        $scriptBlock = {
+            Import-Module 'C:\Program Files\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1';
+            try {
+                ## ErrorAction is ignored :@
+                $xdSite = Get-XDSite;
+                $xdCustomSite = [PSCustomObject] @{
+                    SiteName = $xdSite.Name;
+                    DatabaseServer = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty ServerAddress;
+                    SiteDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty Name;
+                    LoggingDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Logging | Select-Object -ExpandProperty Name;
+                    MonitorDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Monitor | Select-Object -ExpandProperty Name;
+                };
+                return $xdCustomSite;
+            }
+            catch {
+                Write-Error $_;
+            }
+        };
+        $invokeCommandParams = @{
+            ComputerName = $env:COMPUTERNAME;
+            Credential = $Credential;
+            Authentication = 'Credssp';
+            ScriptBlock = $scriptBlock;
+            ErrorAction = 'SilentlyContinue';
         }
-        catch {
-            
-        }
-        $targetResource = @{
-            SiteName = $xdSite.Name;
-            DatabaseServer = $DatabaseServer;
-            SiteDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty Name;
-            LoggingDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Logging | Select-Object -ExpandProperty Name;
-            MonitorDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Monitor | Select-Object -ExpandProperty Name;
-        }
+        Write-Verbose $localizedData.InvokingScriptBlock;
+        $targetResource = Invoke-Command @invokeCommandParams;
         return $targetResource;
     } #end process
 } #end function Get-TargetResource
@@ -42,15 +56,16 @@ function Test-TargetResource {
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $DatabaseServer,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $SiteDatabaseName,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $LoggingDatabaseName,
-        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName,
+        [Parameter(Mandatory)] [ValidateNotNull()] [System.Management.Automation.PSCredential] $Credential
     )
     process {
         $targetResource = Get-TargetResource @PSBoundParameters;
-        if ($targetResource.SiteName -ne $SiteName) { return $true; }
-        elseif ($targetResource.SiteDatabaseName -ne $SiteDatabaseName) { return $true; }
-        elseif ($targetResource.LoggingDatabaseName -ne $LoggingDatabaseName) { return $true; }
-        elseif ($targetResource.MonitorDatabaseName -ne $MonitorDatabaseName) { return $true; }
-        else { return $false; }
+        if ($targetResource.SiteName -ne $SiteName) { return $false; }
+        elseif ($targetResource.SiteDatabaseName -ne $SiteDatabaseName) { return $false; }
+        elseif ($targetResource.LoggingDatabaseName -ne $LoggingDatabaseName) { return $false; }
+        elseif ($targetResource.MonitorDatabaseName -ne $MonitorDatabaseName) { return $false; }
+        return $true;
     } #end process
 } #end function Test-TargetResource
 
@@ -61,22 +76,47 @@ function Set-TargetResource {
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $DatabaseServer,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $SiteDatabaseName,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $LoggingDatabaseName,
-        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName,
+        [Parameter(Mandatory)] [ValidateNotNull()] [System.Management.Automation.PSCredential] $Credential
     )
     begin {
-        if (-not (TestModule -Name 'Citrix.XenDesktop.Admin')) {
+        if (-not (TestXDModule)) {
             ThrowInvalidProgramException -ErrorId 'Citrix.XenDesktop.Admin module not found.' -ErrorMessage $localizedData.XenDesktopSDKNotFoundError;
         }
     } #end begin
     process {
-        $newXDSiteParams = @{
-            SiteName = $SiteName;
-            DatabaseServer = $DatabaseServer;
-            SiteDatabaseName = $SiteDatabaseName;
-            LoggingDatabaseName = $LoggingDatabaseName;
-            MonitorDatabaseName = $MonitorDatabaseName;
+        $scriptBlock = {
+            param (
+                [System.String] $SiteName,
+                [System.String] $DatabaseServer,
+                [System.String] $SiteDatabaseName,
+                [System.String] $LoggingDatabaseName,
+                [System.String] $MonitorDatabaseName
+            )
+            try {
+                Import-Module 'C:\Program Files\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1';
+                $newXDSiteParams = @{
+                    SiteName = $SiteName;
+                    DatabaseServer = $DatabaseServer;
+                    SiteDatabaseName = $SiteDatabaseName;
+                    LoggingDatabaseName = $LoggingDatabaseName;
+                    MonitorDatabaseName = $MonitorDatabaseName;
+                }
+                $xdSite = New-XDSite @newXDSiteParams -ErrorAction SilentlyContinue;
+            }
+            catch {
+                Write-Error $_;
+            }
+        } #end scriptBLock
+        $invokeCommandParams = @{
+            ComputerName = $env:COMPUTERNAME;
+            Credential = $Credential;
+            Authentication = 'Credssp';
+            ScriptBlock = $scriptBlock;
+            ArgumentList = @($SiteName, $DatabaseServer, $SiteDatabaseName, $LoggingDatabaseName, $MonitorDatabaseName);
+            ErrorAction = 'Stop';
         }
-        Write-Verbose ($localizedData.CreatingXDSite -f $DataStore, $DatabaseName, $DatabaseServer);
-        $xdSite = New-XDSite @newXDSiteParams;
+        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", $invokeCommandParams['ArgumentList']));
+        $invokeCommandResult = Invoke-Command @invokeCommandParams;
     } #end process
 } #end function Test-TargetResource
