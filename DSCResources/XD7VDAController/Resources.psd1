11a0e518c92cb649edd0a9cd4d8322af0a0d244b
Iain Brighton <iainbrighton@hotmail.com>
2015-05-08 18:18:49 +0100

    Added XD7VDAController resource

---
+++ DSCResources/XD7VDAController/Resources.psd1
@@ -0,0 +1,8 @@
+ConvertFrom-StringData @'    
+    MissingDeliveryController = Citrix XenDesktop 7.x Delivery Controller '{0}' is missing and will be added.
+    AdditionalDeliveryController = Citrix XenDesktop 7.x Delivery Controller '{0}' is present and will be removed.
+    AddingDeliveryController = Adding Citrix XenDesktop 7.x '{0}' Delivery Controller.
+    RemovingDeliveryController = Removing Citrix XenDesktop 7.x '{0}' Delivery Controller.
+    SettingRegSZValue = Setting registry REG_SZ '{0}' value with '{1}'.
+    RestartingService = Restarting Citrix XenDesktop 7.x '{0}' service.
+'@
---
+++ DSCResources/XD7VDAController/XD7VDAController.Tests.ps1
@@ -0,0 +1,60 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".");
+. "$here\$sut";
+
+## Dot source XD7Common functions
+$moduleParent = Split-Path -Path $here -Parent;
+Get-ChildItem -Path "$moduleParent\XD7Common" -Include *.ps1 -Exclude '*.Tests.ps1' -Recurse |
+    ForEach-Object { . $_.FullName; }
+
+Describe 'cXD7VDAController\Test-TargetResource' {
+
+    Context 'Ensure Present' {
+
+        It 'returns $true when DDC exists with single DDC.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc1'; }
+            Test-TargetResource -Name 'ddc1' -Ensure 'Present' | Should Be $true;
+        }
+
+        It 'returns $true when DDC exists with mulitple DDCs.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc1 ddc2'; }
+            Test-TargetResource -Name 'ddc1' -Ensure 'Present' | Should Be $true;
+        }
+
+        It 'returns $false when DDC does not exist with single DDCs.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc2'; }
+            Test-TargetResource -Name 'ddc1' -Ensure 'Present' | Should Be $false;
+        }
+
+        It 'returns $false when DDC does not exist with mulitple DDCs.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc1 ddc2'; }
+            Test-TargetResource -Name 'ddc3' -Ensure 'Present' | Should Be $false;
+        }
+
+    } #end context ensure present
+
+    Context 'Ensure Absent' {
+
+        It 'returns $false when DDC exists with single DDC.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc1'; }
+            Test-TargetResource -Name 'ddc1' -Ensure 'Absent' | Should Be $false;
+        }
+
+        It 'returns $false when DDC exists with mulitple DDCs.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc1 ddc2'; }
+            Test-TargetResource -Name 'ddc1' -Ensure 'Absent' | Should Be $false;
+        }
+
+        It 'returns $true when DDC does not exist with single DDCs.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc2'; }
+            Test-TargetResource -Name 'ddc1' -Ensure 'Absent' | Should Be $true;
+        }
+
+        It 'returns $true when DDC does not exist with mulitple DDCs.' {
+            Mock -CommandName GetRegistryValue -MockWith { return 'ddc1 ddc2'; }
+            Test-TargetResource -Name 'ddc3' -Ensure 'Absent' | Should Be $true;
+        }
+
+    } #end context ensure absent
+    
+} #end describe cXD7VDAController\Test-TargetResource
