472a846e65ab9fd1e67ea99574cd15980cf0b046
Iain Brighton <iainbrighton@hotmail.com>
2015-05-08 18:19:21 +0100

    Removed XD7VDA resource

--- DSCResources/XD7VDA/XD7VDA.Tests.ps1
+++
@@ -1,9 +0,0 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path;
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".");
-. "$here\$sut";
-
-## Dot source XD7Common functions
-$moduleParent = Split-Path -Path $here -Parent;
-Get-ChildItem -Path "$moduleParent\XD7Common" -Include *.ps1 -Exclude '*.Tests.ps1' -Recurse |
-    ForEach-Object { . $_.FullName; }
-
