2547d0f7057886705ff1f8135a6ce5ef2c36d40a
iainbrighton <iainbrighton@hotmail.com>
2015-05-24 20:48:25 +0100

    Added Credential parameter to XD7SiteLicence resource Implemented $using syntax for DSC v1.0 and v2.0 compatibility

--- DSCResources/XD7SiteLicense/XD7SiteLicense.ps1
+++ DSCResources/XD7SiteLicense/XD7SiteLicense.ps1
@@ -18,12 +18,6 @@ function Get-TargetResource {
     }
     process {
         $scriptBlock = {
-            param (
-                [System.String] $LicenseServer,
-                [System.String] $LicenseServerPort,
-                [System.String] $LicenseEdition,
-                [System.String] $LicenseModel
-            )
             $VerbosePreference = 'Continue';
             Add-PSSnapin -Name 'Citrix.Configuration.Admin.V2' -ErrorAction Stop;
             try {
@@ -35,27 +29,19 @@ function Get-TargetResource {
                 LicenseServerPort = $xdSiteConfig.LicenseServerPort;
                 LicenseEdition = $xdSiteConfig.ProductEdition;
                 LicenseModel = $xdSiteConfig.LicensingModel;
-                Ensure = 'Present';
+                Ensure = $using:Ensure;
             };
-            foreach ($parameterKey in $PSBoundParameters.Keys) {
-                if ($xdCurrentSite[$parameterKey] -ne $PSBoundParameters[$parameterKey]) {
-                    $xdCurrentSite['Ensure'] = 'Absent';
-                    $PropertyMismatchMessage = 'Mismatch on property "{0}": "{1}" <> "{2}". Setting "Ensure" to "Absent".'
-                    Write-Verbose ($PropertyMismatchMessage -f $parameterKey, $xdCurrentSite[$parameterKey], $PSBoundParameters[$parameterKey]);
-                }
-            }
-            return [PSCustomObject] $xdCurrentSite;
+            return $xdCurrentSite;
         } #end scriptblock
+
         $invokeCommandParams = @{
             ScriptBlock = $scriptBlock;
-            ArgumentList = @($LicenseServer, $LicenseServerPort, $LicenseEdition, $LicenseModel);
             ErrorAction = 'Stop';
         }
-        if ($Credential) {
-            AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential;
-        }
-        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", $invokeCommandParams['ArgumentList']));
-        $targetResource = Invoke-Command @invokeCommandParams;
+        if ($Credential) { AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential; }
+        else { $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$')); }
+        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", @($LicenseServer, $LicenseServerPort, $LicenseEdition, $LicenseModel)));
+        $targetResource = Invoke-Command  @invokeCommandParams;
         return $targetResource;
     } #end process
 } #end function Get-TargetResource
@@ -65,26 +51,33 @@ function Test-TargetResource {
     [OutputType([System.Boolean])]
     param (
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $LicenseServer,
-        [Parameter(Mandatory)] [AllowNull()] [System.Management.Automation.PSCredential] $Credential,
+        [Parameter()] [AllowNull()] [System.Management.Automation.PSCredential] $Credential,
         [Parameter()] [ValidateSet('Present','Absent')] [System.String] $Ensure = 'Present',
         [Parameter()] [ValidateNotNull()] [System.Int16] $LicenseServerPort = 27000,
         [Parameter()] [ValidateSet('PLT','ENT','APP')] [System.String] $LicenseEdition = 'PLT',
         [Parameter()] [ValidateSet('UserDevice','Concurrent')] [System.String] $LicenseModel = 'UserDevice'
     )
     process {
+        $isInDesiredState = $true;
         if ($Ensure -eq 'Absent') {
             ## Not supported and we will always return $true
             Write-Warning $localizedData.RemovingLicenseServerPropertiesWarning;
         }
         else {
             $targetResource = Get-TargetResource @PSBoundParameters;
-            if (Compare-Object -ReferenceObject $targetResource -DifferenceObject $PSBoundParameters -Property LicenseServer, LicenseServerPort, LicenseEdition, LicenseModel, Ensure) {
+            if ($LicenseServer -ne $targetResource['LicenseServer']) { $isInDesiredState = $false }
+            elseif ($LicenseServerPort -ne $targetResource['LicenseServerPort']) { $isInDesiredState = $false }
+            elseif ($LicenseEdition-ne $targetResource['LicenseEdition']) { $isInDesiredState = $false }
+            elseif ($LicenseModel -ne $targetResource['LicenseModel']) { $isInDesiredState = $false }
+            if ($isInDesiredState) {
+                Write-Verbose $localizedData.ResourceInDesiredState;
+            }
+            else {
                 Write-Verbose $localizedData.ResourceNotInDesiredState;
-                return $false;
             }
         }
-        Write-Verbose $localizedData.ResourceInDesiredState;
-        return $true;
+        
+        return $isInDesiredState;
     } #end process
 } #end function Test-TargetResource
 
@@ -92,7 +85,7 @@ function Set-TargetResource {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $LicenseServer,
-        [Parameter(Mandatory)] [AllowNull()] [System.Management.Automation.PSCredential] $Credential,
+        [Parameter()] [AllowNull()] [System.Management.Automation.PSCredential] $Credential,
         [Parameter()] [ValidateSet('Present','Absent')] [System.String] $Ensure = 'Present',
         [Parameter()] [ValidateNotNull()] [System.Int16] $LicenseServerPort = 27000,
         [Parameter()] [ValidateSet('PLT','ENT','APP')] [System.String] $LicenseEdition = 'PLT',
@@ -106,30 +99,22 @@ function Set-TargetResource {
     process {
         Write-Verbose ($localizedData.SettingLicenseServerProperties -f $Server, $Port, $Edition);
         $scriptBlock = {
-            param (
-                [System.String] $LicenseServer,
-                [System.String] $LicenseServerPort,
-                [System.String] $LicenseEdition,
-                [System.String] $LicenseModel
-            )
             Add-PSSnapin -Name 'Citrix.Configuration.Admin.V2' -ErrorAction Stop;
             $setConfigSiteParams = @{
-                LicenseServerName = $LicenseServer;
-                LicenseServerPort = $LicenseServerPort;
-                ProductEdition = $LicenseEdition;
-                LicensingModel = $LicenseModel;
+                LicenseServerName = $using:LicenseServer;
+                LicenseServerPort = $using:LicenseServerPort;
+                ProductEdition = $using:LicenseEdition;
+                LicensingModel = $using:LicenseModel;
             }
             $xdConfigSite = Set-ConfigSite @setConfigSiteParams;
         } #end scriptBlock
         $invokeCommandParams = @{
             ScriptBlock = $scriptBlock;
-            ArgumentList = @($LicenseServer, $LicenseServerPort, $LicenseEdition, $LicenseModel);
             ErrorAction = 'Stop';
         }
-        if ($Credential) {
-            AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential;
-        }
-        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", $invokeCommandParams['ArgumentList']));
-        $invokeCommandResult = Invoke-Command @invokeCommandParams;
+        if ($Credential) { AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential; }
+        else { $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$')); }
+        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", @($LicenseServer, $LicenseServerPort, $LicenseEdition, $LicenseModel)));
+        Invoke-Command @invokeCommandParams;
     } #end process
 } #end function Set-TargetResource
