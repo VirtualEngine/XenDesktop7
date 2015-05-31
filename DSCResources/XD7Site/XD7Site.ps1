5c97d4244c577d406cf14063103f21f6255d5d51
iainbrighton <iainbrighton@hotmail.com>
2015-05-31 20:31:09 +0100

    Removed Mandatory attribute from Credential parameter Removed verbosity from Import-Module

--- DSCResources/XD7Site/XD7Site.ps1
+++ DSCResources/XD7Site/XD7Site.ps1
@@ -9,7 +9,7 @@ function Get-TargetResource {
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $SiteDatabaseName,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $LoggingDatabaseName,
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [System.String] $MonitorDatabaseName,
-        [Parameter(Mandatory)] [ValidateNotNull()] [System.Management.Automation.PSCredential] $Credential
+        [Parameter()] [ValidateNotNull()] [System.Management.Automation.PSCredential] $Credential
     )
     begin {
         if (-not (TestXDModule)) {
@@ -18,21 +18,19 @@ function Get-TargetResource {
     } #end begin
     process {
         $scriptBlock = {
-            $VerbosePreference = 'SilentlyContinue';
-            Import-Module "$env:ProgramFiles\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1";
-            $VerbosePreference = 'Continue';
+            Import-Module "$env:ProgramFiles\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1" -Verbose:$false;
             try {
                 $xdSite = Get-XDSite;
             }
             catch { }
-            $xdCustomSite = [PSCustomObject] @{
+            $targetResource = @{
                 SiteName = $xdSite.Name;
                 DatabaseServer = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty ServerAddress;
                 SiteDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty Name;
                 LoggingDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Logging | Select-Object -ExpandProperty Name;
                 MonitorDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Monitor | Select-Object -ExpandProperty Name;
             };
-            return $xdCustomSite;
+            return $targetResource;
         };
 
         $invokeCommandParams = @{
@@ -42,8 +40,7 @@ function Get-TargetResource {
         if ($Credential) { AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential; }
         else { $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$')); }
         Write-Verbose $localizedData.InvokingScriptBlock;
-        $targetResource = Invoke-Command @invokeCommandParams;
-        return $targetResource;
+        return Invoke-Command @invokeCommandParams;
     } #end process
 } #end function Get-TargetResource
 
@@ -92,9 +89,7 @@ function Set-TargetResource {
     } #end begin
     process {
         $scriptBlock = {
-            $VerbosePreference = 'SilentlyContinue';
-            Import-Module "$env:ProgramFiles\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1";
-            $VerbosePreference = 'Continue';
+            Import-Module "$env:ProgramFiles\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1" -Verbose:$false;
 
             $newXDSiteParams = @{
                 SiteName = $using:SiteName;
@@ -103,7 +98,7 @@ function Set-TargetResource {
                 LoggingDatabaseName = $using:LoggingDatabaseName;
                 MonitorDatabaseName = $using:MonitorDatabaseName;
             }
-            $xdSite = New-XDSite @newXDSiteParams -ErrorAction SilentlyContinue;
+            $xdSite = New-XDSite @newXDSiteParams;
         } #end scriptBlock
 
         $invokeCommandParams = @{
