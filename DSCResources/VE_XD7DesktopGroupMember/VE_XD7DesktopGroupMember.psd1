b80ec7d70e455c7ae5e512ab4cbec31149878b44
Iain Brighton <iainbrighton@hotmail.com>
2016-06-21 10:10:25 +0100

    Fixes PSSA errors in VE_XD7DesktopGroupMember

---
+++ DSCResources/VE_XD7DesktopGroupMember/VE_XD7DesktopGroupMember.psd1
@@ -0,0 +1,9 @@
+<# VE_XD7DesktopGroupMember\VE_XD7DesktopGroupMember.psd1 #>
+ConvertFrom-StringData @'
+    XenDesktopSDKNotFoundError    = Citrix XenDesktop 7.x Powershell SDK/Snap-in was not found.
+    InvokingScriptBlockWithParams = Invoking script block with parameters: '{0}'.
+    AddingDeliveryGroupMachine    = Adding Citrix XenDesktop 7.x Machine '{0}' to Delivery Group '{1}'.
+    RemovingDeliveryGroupMachine  = Removing Citrix XenDesktop 7.x Machine '{0}' from Delivery Group '{1}'.
+    ResourceInDesiredState        = Citrix XenDesktop 7.x resource '{0}' is in the desired state.
+    ResourceNotInDesiredState     = Citrix XenDesktop 7.x resource '{0}' is NOT in the desired state.
+'@
--- DSCResources/VE_XD7DesktopGroupMember/VE_XD7DesktopGroupMember.psm1
+++ DSCResources/VE_XD7DesktopGroupMember/VE_XD7DesktopGroupMember.psm1
@@ -1,14 +1,162 @@
+Import-LocalizedData -BindingVariable localizedData -FileName VE_XD7DesktopGroupMember.psd1;
+
+function Get-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param (
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $Name,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String[]] $Members,
+
+        [Parameter()] [AllowNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential,
+
+        [Parameter()] [ValidateSet('Present','Absent')]
+        [System.String] $Ensure = 'Present'
+    )
+    begin {
+        AssertXDModule -Name 'Citrix.Broker.Admin.V2' -IsSnapin;
+    } #end begin
+    process {
+
+        $scriptBlock = {
+            Add-PSSnapin -Name 'Citrix.Broker.Admin.V2';
+            $targetResource = @{
+                Name = $using:Name;
+                Members = @();
+                Ensure = 'Absent';
+            }
+            $targetResource['Members'] = Get-BrokerMachine -DesktopGroupName $using:Name | Select-Object -ExpandProperty DnsName;
+            if ($targetResource['Members']) {
+                $targetResource['Ensure'] = 'Present';
+            }
+            return $targetResource;
+        } #end scriptBlock
+
+        $invokeCommandParams = @{
+            ScriptBlock = $scriptBlock;
+            ErrorAction = 'Stop';
+        }
+        if ($Credential) {
+            AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential;
+        }
+        else {
+            $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$'));
+        }
+        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", @($Name, $Members, $Ensure)));
+        return Invoke-Command @invokeCommandParams;
+
+    } #end process
+} #end function Get-TargetResource
+
+
+function Test-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param (
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $Name,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String[]] $Members,
+
+        [Parameter()] [AllowNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential,
+
+        [Parameter()] [ValidateSet('Present','Absent')]
+        [System.String] $Ensure = 'Present'
+    )
+    process {
+
+        $targetResource = Get-TargetResource @PSBoundParameters;
+        if (TestXDMachineMembership -RequiredMembers $Members -ExistingMembers $targetResource.Members -Ensure $Ensure) {
+            Write-Verbose ($localizedData.ResourceInDesiredState -f $Name);
+            return $true;
+        }
+        else {
+            Write-Verbose ($localizedData.ResourceNotInDesiredState -f $Name);
+            return $false;
+        }
+
+    } #end process
+} #end function Get-TargetResource
+
+
+function Set-TargetResource {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
+    param (
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $Name,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String[]] $Members,
+
+        [Parameter()] [AllowNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential,
+
+        [Parameter()] [ValidateSet('Present','Absent')]
+        [System.String] $Ensure = 'Present'
+    )
+    begin {
+        AssertXDModule -Name 'Citrix.Broker.Admin.V2' -IsSnapin;
+    }
+    process {
+
+        $scriptBlock = {
+            Add-PSSnapin -Name 'Citrix.Broker.Admin.V2';
+            Import-Module "$env:ProgramFiles\WindowsPowerShell\Modules\XenDesktop7\DSCResources\VE_XD7Common\VE_XD7Common.psd1" -Verbose:$false;
+
+            $brokerMachines = Get-BrokerMachine -DesktopGroupName $using:Name;
+            foreach ($member in $using:Members) {
+                $brokerMachine = ResolveXDBrokerMachine -MachineName $member -BrokerMachines $brokerMachines;
+                if (($using:Ensure -eq 'Absent') -and ($brokerMachine.DesktopGroupName -eq $using:Name)) {
+                    Write-Verbose ($using:localizedData.RemovingDeliveryGroupMachine -f $member, $using:Name);
+                    $brokerMachine | Remove-BrokerMachine -DesktopGroup $using:Name -Force;
+                }
+                elseif (($using:Ensure -eq 'Present') -and ($brokerMachine.DesktopGroupName -ne $using:Name)) {
+                    $brokerMachine = GetXDBrokerMachine -MachineName $member;
+                    if ($null -eq $brokerMachine) {
+                        ThrowInvalidOperationException -ErrorId 'MachineNotFound' -Message ($localizedData.MachineNotFoundError -f $member);
+                    }
+                    else {
+                        Write-Verbose ($using:localizedData.AddingDeliveryGroupMachine -f $member, $using:Name);
+                        $brokerMachine | Add-BrokerMachine -DesktopGroup $using:Name;
+                    }
+                }
+            } #end foreach member
+        } #end scriptBlock
+
+        $invokeCommandParams = @{
+            ScriptBlock = $scriptBlock;
+            ErrorAction = 'Stop';
+        }
+        if ($Credential) {
+            AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential;
+        }
+        else {
+            $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$'));
+        }
+        $scriptBlockParams = @($Name, $Members, $Ensure);
+        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", $scriptBlockParams));
+        [ref] $null = Invoke-Command @invokeCommandParams;
+
+    } #end process
+} #end function Set-TargetResource
+
+
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent;
 
 ## Import the XD7Common library functions
 $moduleParent = Split-Path -Path $moduleRoot -Parent;
 Import-Module (Join-Path -Path $moduleParent -ChildPath 'VE_XD7Common');
-
-## Dot source all (nested) .ps1 files in the folder, excluding Pester tests
-Get-ChildItem -Path $moduleRoot -Include *.ps1 -Exclude '*.Tests.ps1' -Recurse |
-    ForEach-Object {
-        Write-Verbose ('Dot sourcing ''{0}''.' -f $_.FullName);
-        . $_.FullName;
-    }
 
 Export-ModuleMember -Function *-TargetResource;
