5f1ee682a9dd8ea7d037dbec3f08755bb36eb6a8
Iain Brighton <iainbrighton@hotmail.com>
2016-06-21 09:55:12 +0100

    Fixes PSSA errors in VE_XD7Common

--- DSCResources/VE_XD7Common/VE_XD7Common.psm1
+++ DSCResources/VE_XD7Common/VE_XD7Common.psm1
@@ -3,10 +3,10 @@ Import-LocalizedData -BindingVariable localized -FileName Resources.psd1;
 #region Private Functions
 
 function AddInvokeScriptBlockCredentials {
-    <#
+<#
     .SYNOPSIS
         Adds the required Invoke-Command parameters for loopback processing with CredSSP.
-    #>
+#>
     [CmdletBinding()]
     [OutputType([System.Collections.Hashtable])]
     param (
@@ -19,17 +19,21 @@ function AddInvokeScriptBlockCredentials {
         $Credential
     )
     process {
+
         $Hashtable['ComputerName'] = $env:COMPUTERNAME;
         $Hashtable['Credential'] = $Credential;
         $Hashtable['Authentication'] = 'Credssp';
+
     }
 } #end function AddInvokeScriptBlockCredentials
+
 
 function GetHostname {
     [CmdletBinding()]
     [OutputType([System.String])]
     param ( )
     process {
+
         $globalIpProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties();
         if ($globalIpProperties.DomainName) {
             return '{0}.{1}' -f $globalIpProperties.HostName, $globalIpProperties.DomainName;
@@ -37,18 +41,22 @@ function GetHostname {
         else {
             return $globalIpProperties.HostName;
         }
+
     } #end process
 } #end function GetHostname
 
+
 function GetRegistryValue {
-    <#
+<#
     .SYNOPSIS
         Returns a registry string value.
     .NOTES
         This is an internal function and shouldn't be called from outside.
         This function enables registry calls to be unit tested.
-    #>
+#>
     [CmdletBinding(SupportsShouldProcess)]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '')]
+    [OutputType([System.String])]
     param (
         # Registry key name/path to query.
         [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
@@ -59,21 +67,24 @@ function GetRegistryValue {
         [System.String] $Name
     )
     process {
+
         $itemProperty = Get-ItemProperty -Path $Key -Name $Name -ErrorAction SilentlyContinue;
         if ($itemProperty.$Name) {
             return $itemProperty.$Name;
         }
         return '';
+
     }
 } #end function GetRegistryValue
 
+
 function StartWaitProcess {
-    <#
+<#
     .SYNOPSIS
         Starts and waits for a process to exit.
     .NOTES
         This is an internal function and shouldn't be called from outside.
-    #>
+#>
     [CmdletBinding(SupportsShouldProcess)]
     [OutputType([System.Int32])]
     param (
@@ -96,6 +107,7 @@ function StartWaitProcess {
         [System.String] $WorkingDirectory = (Split-Path -Path $FilePath -Parent)
     )
     process {
+
         $startProcessParams = @{
             FilePath = $FilePath;
             WorkingDirectory = $WorkingDirectory;
@@ -123,14 +135,16 @@ function StartWaitProcess {
             Write-Verbose ($localized.ProcessExited -f $process.Id, $exitCode);
         }
         return $exitCode;
+
     } #end process
 } #end function StartWaitProcess
 
+
 function FindXDModule {
-    <#
+<#
     .SYNOPSIS
         Locates a module's manifest (.psd1) file.
-    #>
+#>
     [CmdletBinding()]
     [OutputType([System.String])]
     param (
@@ -141,21 +155,25 @@ function FindXDModule {
         [System.String] $Path = 'C:\Program Files\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1'
     )
     process {
+
         $module = Get-ChildItem -Path $Path -Include "$Name.psd1" -File -Recurse;
         if (-not $module) {
             # If we have no .psd1 file, search for a .psm1 (for StoreFront)
             $module = Get-ChildItem -Path $Path -Include "$Name.psm1" -File -Recurse;
         }
         return $module.FullName;
+
     } #end process
 } #end function FindModule
 
+
 function TestXDModule {
-    <#
+<#
     .SYNOPSIS
         Tests whether Powershell modules or Snapin are available/registered.
-    #>
+#>
     [CmdletBinding()]
+    [OutputType([System.Boolean])]
     param (
         [Parameter()] [ValidateNotNullOrEmpty()]
         [System.String] $Name = 'Citrix.XenDesktop.Admin',
@@ -167,6 +185,7 @@ function TestXDModule {
         [System.Management.Automation.SwitchParameter] $IsSnapin
     )
     process {
+
         if ($IsSnapin) {
             if (Get-PSSnapin -Name $Name -Registered) {
                 return $true;
@@ -178,14 +197,16 @@ function TestXDModule {
             }
         }
         return $false;
+
     } #end process
 } #end TestModule
 
+
 function AssertXDModule {
-    <#
+<#
     .SYNOPSIS
         Asserts whether all the specified modules are present, throwing if not.
-    #>
+#>
     [CmdletBinding()]
     param (
         [Parameter()] [ValidateNotNullOrEmpty()]
@@ -198,53 +219,64 @@ function AssertXDModule {
         [System.Management.Automation.SwitchParameter] $IsSnapin
     )
     process {
+
         foreach ($moduleName in $Name) {
             if (-not (TestXDModule -Name $moduleName -Path $Path -IsSnapin:$IsSnapin)) {
                 ThrowInvalidProgramException -ErrorId $moduleName -ErrorMessage $localized.XenDesktopSDKNotFoundError;
             }
         } #end foreach module
+
     } #end process
 } #end function AssertXDModule
 
+
 function GetXDBrokerMachine {
-    <#
+<#
+    .SYNOPSIS
         Searches for a registered Citrix XenDesktop machine by name.
-    #>
+#>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
         [System.String] $MachineName
     )
-    if ($MachineName.Contains('.')) {
-        ## Attempt to locate the machine by FQDN
-        $brokerMachine = Get-BrokerMachine -DNSName $MachineName -ErrorAction SilentlyContinue;
-    }
-    elseif ($MachineName.Contains('\')) {
-        ## Otherwise attempt to locate the machine by DomainName\NetBIOSName
-        $brokerMachine = Get-BrokerMachine -MachineName $MachineName -ErrorAction SilentlyContinue;
-    }
-    else {
-        ## Failing all else, perform a wildcard search
-        $brokerMachine = Get-BrokerMachine -MachineName "*\$MachineName" -ErrorAction SilentlyContinue;
-    }
-    if ($brokerMachine -eq $null) {
-        Write-Error -ErrorId 'MachineNotFound' -Message ($localized.MachineNotFoundError -f $Machine);
-        return;
-    }
-    elseif (($brokerMachine).Count -gt 1) {
-        Write-Error -ErrorId 'AmbiguousMachineReference' -Message ($localized.AmbiguousMachineReferenceError -f $MachineName);
-        return;
-    }
-    else {
-        return $brokerMachine;
-    }
-}
+    process {
+
+        if ($MachineName.Contains('.')) {
+            ## Attempt to locate the machine by FQDN
+            $brokerMachine = Get-BrokerMachine -DNSName $MachineName -ErrorAction SilentlyContinue;
+        }
+        elseif ($MachineName.Contains('\')) {
+            ## Otherwise attempt to locate the machine by DomainName\NetBIOSName
+            $brokerMachine = Get-BrokerMachine -MachineName $MachineName -ErrorAction SilentlyContinue;
+        }
+        else {
+            ## Failing all else, perform a wildcard search
+            $brokerMachine = Get-BrokerMachine -MachineName "*\$MachineName" -ErrorAction SilentlyContinue;
+        }
+        if ($null -eq $brokerMachine) {
+            Write-Error -ErrorId 'MachineNotFound' -Message ($localized.MachineNotFoundError -f $Machine);
+            return;
+        }
+        elseif (($brokerMachine).Count -gt 1) {
+            Write-Error -ErrorId 'AmbiguousMachineReference' -Message ($localized.AmbiguousMachineReferenceError -f $MachineName);
+            return;
+        }
+        else {
+            return $brokerMachine;
+        }
+
+    } #end process
+} #end function GetXDBrokerMachine
+
 
 function TestXDMachineIsExistingMember {
-    <#
+<#
+    .SYNOPSIS
         Tests whether a machine is an existing member of a list of FQDN machine members.
-    #>
+#>
     [CmdletBinding()]
+    [OutputType([System.Boolean])]
     param (
         [Parameter(Mandatory)]
         [System.String] $MachineName,
@@ -252,30 +284,38 @@ function TestXDMachineIsExistingMember {
         [Parameter()]
         [System.String[]] $ExistingMembers
     )
-    if ((-not $MachineName.Contains('\')) -and (-not $MachineName.Contains('.'))) {
-        Write-Warning -Message ($localized.MachineNameNotFullyQualifiedWarning -f $MachineName);
-        $netBIOSName = $MachineName;
-    }
-    elseif ($MachineName.Contains('\')) {
-        $netBIOSName = $MachineName.Split('\')[1];
-    }
+    process {
 
-    if ($ExistingMembers -contains $MachineName) {
-        return $true;
-    }
-    elseif ($ExistingMembers -like '{0}.*' -f $netBIOSName) {
-        return $true;
-    }
-    else {
-        return $false;
-    }
+        if ((-not $MachineName.Contains('\')) -and (-not $MachineName.Contains('.'))) {
+            Write-Warning -Message ($localized.MachineNameNotFullyQualifiedWarning -f $MachineName);
+            $netBIOSName = $MachineName;
+        }
+        elseif ($MachineName.Contains('\')) {
+            $netBIOSName = $MachineName.Split('\')[1];
+        }
+
+        if ($ExistingMembers -contains $MachineName) {
+            return $true;
+        }
+        elseif ($ExistingMembers -like '{0}.*' -f $netBIOSName) {
+            return $true;
+        }
+        else {
+            return $false;
+        }
+
+    } #end process
 } #end function TestXDMachine
 
+
 function TestXDMachineMembership {
-    <#
+<#
+    .SYNOPSIS
         Provides a centralised function to test whether machine membership of a Machine Catalog or
         Delivery Group are correct - evaluating FQDNs, DOMAINNAME\NETBIOS and NETBIOS name formats.
-    #>
+#>
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
     param (
         [Parameter(Mandatory)]
         [System.String[]] $RequiredMembers,
@@ -287,6 +327,7 @@ function TestXDMachineMembership {
         [System.String[]] $ExistingMembers
     )
     process {
+
         $isInCompliance = $true;
         foreach ($member in $RequiredMembers) {
             if (TestXDMachineIsExistingMember -MachineName $member -ExistingMembers $ExistingMembers) {
@@ -303,14 +344,19 @@ function TestXDMachineMembership {
             }
         } #end foreach member
         return $isInCompliance;
+
     } #end process
 } #end function TestXDMachineMembers
 
+
 function ResolveXDBrokerMachine {
-    <#
+<#
+    .SYNOPSIS
         Returns a machine machine from an existing collection of Citrix XenDesktop
         machines assigned to a Machine Catalog or Delivery Group
-    #>
+#>
+    [CmdletBinding()]
+    [OutputType([System.String])]
     param (
         [Parameter(Mandatory)]
         [System.String] $MachineName,
@@ -318,25 +364,31 @@ function ResolveXDBrokerMachine {
         [Parameter(Mandatory)] [AllowNull()]
         [System.Object[]] $BrokerMachines
     )
-    $brokerMachine = $null;
-    foreach ($machine in $brokerMachines) {
-        ## Try matching on DNS name
-        if (($machine.DNSName -eq $MachineName) -or ($machine.MachineName -eq $MachineName)) {
-            return $machine;
-        }
-        elseif ((-not $MachineName.Contains('\')) -and ($machine.MachineName -match '^\S+\\{0}$' -f $MachineName)) {
-            ## Try matching based on DOMAIN\NETBIOS name
-            return $machine
-        }
-    } #end foreach machine
-    return $null;
+    process {
+
+        $brokerMachine = $null;
+        foreach ($machine in $brokerMachines) {
+            ## Try matching on DNS name
+            if (($machine.DNSName -eq $MachineName) -or ($machine.MachineName -eq $MachineName)) {
+                return $machine;
+            }
+            elseif ((-not $MachineName.Contains('\')) -and ($machine.MachineName -match '^\S+\\{0}$' -f $MachineName)) {
+                ## Try matching based on DOMAIN\NETBIOS name
+                return $machine
+            }
+        } #end foreach machine
+        return $null;
+
+    } #end process
 } #end function ResolveXDBrokerMachine
 
+
 function ThrowInvalidOperationException {
-    <#
+<#
     .SYNOPSIS
         Throws terminating error of category NotInstalled with specified errorId and errorMessage.
-    #>
+#>
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [System.String] $ErrorId,
@@ -344,18 +396,23 @@ function ThrowInvalidOperationException {
         [Parameter(Mandatory)]
         [System.String] $ErrorMessage
     )
-    $errorCategory = [System.Management.Automation.ErrorCategory]::InvalidArgument;
-    $exception = New-Object -TypeName 'System.InvalidOperationException' -ArgumentList $ErrorMessage;
-    $errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList $exception, $ErrorId, $errorCategory, $null;
-    throw $errorRecord;
+    process {
 
-}
+        $errorCategory = [System.Management.Automation.ErrorCategory]::InvalidArgument;
+        $exception = New-Object -TypeName 'System.InvalidOperationException' -ArgumentList $ErrorMessage;
+        $errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList $exception, $ErrorId, $errorCategory, $null;
+        throw $errorRecord;
+
+    } #end process
+} #end function ThrowInvalidOperationException
+
 
 function ThrowInvalidProgramException {
-    <#
+<#
     .SYNOPSIS
         Throws terminating error of category NotInstalled with specified errorId and errorMessage.
-    #>
+#>
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [System.String] $ErrorId,
@@ -363,17 +420,22 @@ function ThrowInvalidProgramException {
         [Parameter(Mandatory)]
         [System.String] $ErrorMessage
     )
-    $errorCategory = [System.Management.Automation.ErrorCategory]::NotInstalled;
-    $exception = New-Object -TypeName 'System.InvalidProgramException' -ArgumentList $ErrorMessage;
-    $errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList $exception, $ErrorId, $errorCategory, $null;
-    throw $errorRecord;
+    process {
+
+        $errorCategory = [System.Management.Automation.ErrorCategory]::NotInstalled;
+        $exception = New-Object -TypeName 'System.InvalidProgramException' -ArgumentList $ErrorMessage;
+        $errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList $exception, $ErrorId, $errorCategory, $null;
+        throw $errorRecord;
+
+    } #end process
 } #end function ThrowInvalidProgramException
 
+
 function ThrowOperationCanceledException {
-    <#
+<#
     .SYNOPSIS
         Throws terminating error of category InvalidOperation with specified errorId and errorMessage.
-    #>
+#>
     param(
         [Parameter(Mandatory)]
         [System.String] $ErrorId,
@@ -381,17 +443,22 @@ function ThrowOperationCanceledException {
         [Parameter(Mandatory)]
         [System.String] $ErrorMessage
     )
-    $errorCategory = [System.Management.Automation.ErrorCategory]::InvalidOperation;
-    $exception = New-Object -TypeName 'System.OperationCanceledException' -ArgumentList $ErrorMessage;
-    $errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList $exception, $ErrorId, $errorCategory, $null;
-    throw $errorRecord;
+    process {
+
+        $errorCategory = [System.Management.Automation.ErrorCategory]::InvalidOperation;
+        $exception = New-Object -TypeName 'System.OperationCanceledException' -ArgumentList $ErrorMessage;
+        $errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList $exception, $ErrorId, $errorCategory, $null;
+        throw $errorRecord;
+
+    } #end process
 } #end function ThrowOperationCanceledException
 
+
 function TestXDInstalledRole {
-    <#
+<#
     .SYNOPSIS
         Tests whether a Citrix XenDesktop 7.x role is installed.
-    #>
+#>
     [CmdletBinding()]
     [OutputType([System.Boolean])]
     param (
@@ -400,26 +467,29 @@ function TestXDInstalledRole {
         [System.String] $Role
     )
     process {
+
         if (GetXDInstalledRole -Role $Role) {
             return $true;
         }
         return $false;
+
     } #end process
 } #end function TestXDRole
 
+
 function GetXDInstalledRole {
-    <#
+<#
     .SYNOPSIS
         Returns installed Citrix XenDesktop 7.x installed product by role.
-    #>
+#>
     [CmdletBinding()]
-    [OutputType([System.String])]
     param (
         ## Citrix XenDesktop 7.x role to query.
         [Parameter(Mandatory)] [ValidateSet('Controller','Studio','Storefront','Licensing','Director','DesktopVDA','SessionVDA')]
         [System.String] $Role
     )
     process {
+
         $installedProducts = Get-ItemProperty 'HKLM:\SOFTWARE\Classes\Installer\Products\*' -ErrorAction SilentlyContinue |
             Where-Object { $_.ProductName -like '*Citrix*' -and $_.ProductName -notlike '*snap-in' } |
                 Select-Object -ExpandProperty ProductName;
@@ -453,15 +523,17 @@ function GetXDInstalledRole {
         else {
             return $result;
         }
+
     } #end process
 } #end functoin GetXDInstalledProduct
 
+
 function ResolveXDSetupMedia {
-    <#
+<#
     .SYNOPSIS
         Resolve the correct installation media source for the
         local architecture depending on the role.
-    #>
+#>
     [CmdletBinding()]
     [OutputType([System.String])]
     param (
@@ -474,6 +546,7 @@ function ResolveXDSetupMedia {
         [System.String] $SourcePath
     )
     process {
+
         $architecture = 'x86';
         if ([System.Environment]::Is64BitOperatingSystem) {
             $architecture = 'x64';
@@ -495,6 +568,7 @@ function ResolveXDSetupMedia {
             throw ($localized.NoValidSetupMediaError -f $installMedia, $sourceArchitecturePath);
         }
         return $installMediaPath.FullName;
+
     } #end process
 } #end function ResolveXDSetupMedia
 
