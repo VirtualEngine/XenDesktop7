823b7093ca82cd587e27aa849fc94d7c455656b1
Iain Brighton <iainbrighton@hotmail.com>
2016-06-21 10:22:14 +0100

    Fixes PSSA errors in VE_XD7Site

---
+++ DSCResources/VE_XD7Site/VE_XD7Site.psd1
@@ -0,0 +1,10 @@
+<# VE_XD7Site\VE_XD7Site.psd1 #>
+ConvertFrom-StringData @'
+    XenDesktopSDKNotFoundError    = Citrix XenDesktop 7.x Powershell SDK/Snap-in was not found.
+    CreatingXDSite                = Creating Citrix XenDesktop 7.x Site '{0}'.
+    InvokingScriptBlock           = Invoking script block.
+    ResourcePropertyMismatch      = Property '{0}' is NOT in desired state; expected '{1}', actual '{2}'.
+    ResourceInDesiredState        = Citrix XenDesktop 7.x Site '{0}' is in the desired state.
+    ResourceNotInDesiredState     = Citrix XenDesktop 7.x Site '{0}' is NOT in the desired state.
+    InvokingScriptBlockWithParams = Invoking script block with parameters: '{0}'.
+'@
--- DSCResources/VE_XD7Site/VE_XD7Site.psm1
+++ DSCResources/VE_XD7Site/VE_XD7Site.psm1
@@ -1,14 +1,187 @@
+Import-LocalizedData -BindingVariable localizedData -FileName VE_XD7Site.psd1;
+
+function Get-TargetResource {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingEmptyCatchBlock', '')]
+    [OutputType([System.Collections.Hashtable])]
+    param (
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $SiteName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $DatabaseServer,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $SiteDatabaseName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $LoggingDatabaseName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $MonitorDatabaseName,
+
+        [Parameter()] [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential
+    )
+    begin {
+        AssertXDModule -Name 'Citrix.XenDesktop.Admin';
+    } #end begin
+    process {
+
+        $scriptBlock = {
+            Import-Module "$env:ProgramFiles\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1" -Verbose:$false;
+            try {
+                $xdSite = Get-XDSite;
+            }
+            catch { }
+            $targetResource = @{
+                SiteName = $xdSite.Name;
+                DatabaseServer = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty ServerAddress;
+                SiteDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Site | Select-Object -ExpandProperty Name;
+                LoggingDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Logging | Select-Object -ExpandProperty Name;
+                MonitorDatabaseName = $xdSite.Databases | Where-Object Datastore -eq Monitor | Select-Object -ExpandProperty Name;
+            };
+            return $targetResource;
+        };
+
+        $invokeCommandParams = @{
+            ScriptBlock = $scriptBlock;
+            ErrorAction = 'Stop';
+        }
+        if ($Credential) {
+            AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential;
+        }
+        else {
+            $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$'));
+        }
+        Write-Verbose $localizedData.InvokingScriptBlock;
+        return Invoke-Command @invokeCommandParams;
+
+    } #end process
+} #end function Get-TargetResource
+
+
+function Test-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param (
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $SiteName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $DatabaseServer,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $SiteDatabaseName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $LoggingDatabaseName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $MonitorDatabaseName,
+
+        [Parameter()] [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential
+    )
+    process {
+
+        $targetResource = Get-TargetResource @PSBoundParameters;
+            $inCompliance = $true;
+            foreach ($property in $PSBoundParameters.Keys) {
+                if ($targetResource.ContainsKey($property)) {
+                    $expected = $PSBoundParameters[$property];
+                    $actual = $targetResource[$property];
+                    if ($PSBoundParameters[$property] -is [System.String[]]) {
+                        if (Compare-Object -ReferenceObject $expected -DifferenceObject $actual) {
+                            Write-Verbose ($localizedData.ResourcePropertyMismatch -f $property, ($expected -join ','), ($actual -join ','));
+                            $inCompliance = $false;
+                        }
+                    }
+                    elseif ($expected -ne $actual) {
+                        Write-Verbose ($localizedData.ResourcePropertyMismatch -f $property, $expected, $actual);
+                        $inCompliance = $false;
+                    }
+                }
+            }
+            if ($inCompliance) {
+                Write-Verbose ($localizedData.ResourceInDesiredState -f $DeliveryGroup);
+            }
+            else {
+                Write-Verbose ($localizedData.ResourceNotInDesiredState -f $DeliveryGroup);
+            }
+            return $inCompliance;
+
+    } #end process
+} #end function Test-TargetResource
+
+function Set-TargetResource {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
+    param (
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $SiteName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $DatabaseServer,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $SiteDatabaseName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $LoggingDatabaseName,
+
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $MonitorDatabaseName,
+
+        [Parameter()] [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential
+    )
+    begin {
+        AssertXDModule -Name 'Citrix.XenDesktop.Admin';
+    } #end begin
+    process {
+
+        $scriptBlock = {
+            Import-Module "$env:ProgramFiles\Citrix\XenDesktopPoshSdk\Module\Citrix.XenDesktop.Admin.V1\Citrix.XenDesktop.Admin\Citrix.XenDesktop.Admin.psd1" -Verbose:$false;
+
+            $newXDSiteParams = @{
+                SiteName = $using:SiteName;
+                DatabaseServer = $using:DatabaseServer;
+                SiteDatabaseName = $using:SiteDatabaseName;
+                LoggingDatabaseName = $using:LoggingDatabaseName;
+                MonitorDatabaseName = $using:MonitorDatabaseName;
+            }
+            $xdSite = New-XDSite @newXDSiteParams;
+        } #end scriptBlock
+
+        $invokeCommandParams = @{
+            ScriptBlock = $scriptBlock;
+            ErrorAction = 'Stop';
+        }
+        if ($Credential) {
+            AddInvokeScriptBlockCredentials -Hashtable $invokeCommandParams -Credential $Credential;
+        }
+        else {
+            $invokeCommandParams['ScriptBlock'] = [System.Management.Automation.ScriptBlock]::Create($scriptBlock.ToString().Replace('$using:','$'));
+        }
+        $scriptBlockParams = @($SiteName, $DatabaseServer, $SiteDatabaseName, $LoggingDatabaseName, $MonitorDatabaseName);
+        Write-Verbose ($localizedData.InvokingScriptBlockWithParams -f [System.String]::Join("','", $scriptBlockParams));
+        [ref] $null = Invoke-Command  @invokeCommandParams;
+
+    } #end process
+} #end function Test-TargetResource
+
+
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent;
 
 ## Import the XD7Common library functions
 $moduleParent = Split-Path -Path $moduleRoot -Parent;
 Import-Module (Join-Path -Path $moduleParent -ChildPath 'VE_XD7Common');
-
-## Dot source all (nested) .ps1 files in the folder, excluding Pester tests
-Get-ChildItem -Path $moduleRoot -Include *.ps1 -Exclude '*.Tests.ps1' -Recurse |
-    ForEach-Object {
-        Write-Verbose ('Dot sourcing ''{0}''.' -f $_.FullName);
-        . $_.FullName;
-    }
 
 Export-ModuleMember -Function *-TargetResource;
